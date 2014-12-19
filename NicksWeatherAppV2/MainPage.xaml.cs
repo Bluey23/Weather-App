5fda26ee153cfb9dcc845d2899a56297c456b9cf
Bluey23 <blueythedog3@gmail.com>
2014-12-19 22:20:34 +1030

    My second attempt at a weather app

---
+++ NicksWeatherAppV2/MainPage.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using NicksWeatherAppV2.Resources;
+using System.Threading.Tasks;
+using Windows.Devices.Geolocation;
+using System.IO.IsolatedStorage;
+
+namespace NicksWeatherAppV2
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        Geolocator geo; 
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+            geo = new Geolocator();
+
+            // Sample code to localize the ApplicationBar
+            //BuildLocalizedApplicationBar();
+        }
+
+        // Sample code for building a localized ApplicationBar
+        //private void BuildLocalizedApplicationBar()
+        //{
+        //    // Set the page's ApplicationBar to a new instance of ApplicationBar.
+        //    ApplicationBar = new ApplicationBar();
+
+        //    // Create a new button and set the text value to the localized string from AppResources.
+        //    ApplicationBarIconButton appBarButton = new ApplicationBarIconButton(new Uri("/Assets/AppBar/appbar.add.rest.png", UriKind.Relative));
+        //    appBarButton.Text = AppResources.AppBarButtonText;
+        //    ApplicationBar.Buttons.Add(appBarButton);
+
+        //    // Create a new menu item with the localized string from AppResources.
+        //    ApplicationBarMenuItem appBarMenuItem = new ApplicationBarMenuItem(AppResources.AppBarMenuItemText);
+        //    ApplicationBar.MenuItems.Add(appBarMenuItem);
+        //}
+
+        // When the page is loaded, make sure that you have obtained the users consent to use their location
+        protected override void OnNavigatedTo(System.Windows.Navigation.NavigationEventArgs e)
+        {
+            if (IsolatedStorageSettings.ApplicationSettings.Contains("LocationConsent"))
+            {
+                // User has already opted in or out of Location
+                return;
+            }
+            else
+            {
+                MessageBoxResult result =
+                    MessageBox.Show("This app accesses your phone's location. Is that ok?",
+                    "Location",
+                    MessageBoxButton.OKCancel);
+
+                if (result == MessageBoxResult.OK)
+                {
+                    IsolatedStorageSettings.ApplicationSettings["LocationConsent"] = true;
+                }
+                else
+                {
+                    IsolatedStorageSettings.ApplicationSettings["LocationConsent"] = false;
+                }
+
+                IsolatedStorageSettings.ApplicationSettings.Save();
+            }
+        }
+
+        private void SlideView_OnSelectionChanged(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
---
+++ NicksWeatherAppV2/ViewModel/MainViewModel.cs
@@ -0,0 +1,143 @@
+using System;
+using System.Collections.ObjectModel;
+using System.Net;
+using System.Threading.Tasks;
+using System.Windows;
+using GalaSoft.MvvmLight;
+using Newtonsoft.Json;
+using NicksWeatherAppV2.Model;
+using Windows.Devices.Geolocation;
+
+namespace NicksWeatherAppV2.ViewModel
+{
+    /// <summary>
+    /// This class contains properties that the main View can data bind to.
+    /// <para>
+    /// Use the <strong>mvvminpc</strong> snippet to add bindable properties to this ViewModel.
+    /// </para>
+    /// <para>
+    /// You can also use Blend to data bind with the tool's support.
+    /// </para>
+    /// <para>
+    /// See http://www.galasoft.ch/mvvm
+    /// </para>
+    /// </summary>
+    public class MainViewModel : ViewModelBase
+    {
+        /// <summary>
+        /// The <see cref="DayList" /> property's name.
+        /// </summary>
+        public const string DayListPropertyName = "DayList";
+
+        private ObservableCollection<Day> _dayList = null;
+        Geolocator geo = null;
+
+        /// <summary>
+        /// Sets and gets the DayList property.
+        /// Changes to that property's value raise the PropertyChanged event. 
+        /// </summary>
+        public ObservableCollection<Day> DayList
+        {
+            get
+            {
+                return _dayList;
+            }
+
+            set
+            {
+                if (_dayList == value)
+                {
+                    return;
+                }
+
+                //RaisePropertyChanging(DayListPropertyName);
+                _dayList = value;
+                RaisePropertyChanged(DayListPropertyName);
+            }
+        }
+
+        /// <summary>
+        /// The <see cref="CurrentDay" /> property's name.
+        /// </summary>
+        public const string CurrentDayPropertyName = "CurrentDay";
+
+        private Day _currentDay = null;
+
+        /// <summary>
+        /// Sets and gets the CurrentDay property.
+        /// Changes to that property's value raise the PropertyChanged event. 
+        /// </summary>
+        public Day CurrentDay
+        {
+            get
+            {
+                return _currentDay;
+            }
+
+            set
+            {
+                if (_currentDay == value)
+                {
+                    return;
+                }
+
+                //RaisePropertyChanging(CurrentDayPropertyName);
+                _currentDay = value;
+                RaisePropertyChanged(CurrentDayPropertyName);
+            }
+        }
+
+        public MainViewModel()
+        {
+            if (IsInDesignMode)
+            {
+            }
+            else
+            {
+
+                geo = new Geolocator();
+                getGeoLocation();
+
+            }
+        }
+
+        private void weatherClient_DownloadStringCompleted(object sender, DownloadStringCompletedEventArgs e)
+        {
+            if (e.Error == null)
+            {
+                RootObject result = JsonConvert.DeserializeObject<RootObject>(e.Result);
+                DayList = new ObservableCollection<Day>(result.list);
+                CurrentDay = DayList[0];
+            }
+            else
+            {
+                MessageBox.Show("Error");
+            }
+        }
+
+        async private void getGeoLocation()
+        {
+
+            try
+            {
+                //gets geoposition of phone
+                Geoposition pos = await geo.GetGeopositionAsync();
+                String lat = pos.Coordinate.Latitude.ToString();
+                String lon = pos.Coordinate.Longitude.ToString();
+
+
+                //uses the lon and lat with the weather api to get the weather in the area of the phone 
+                WebClient weatherClient = new WebClient();
+                weatherClient.DownloadStringCompleted += weatherClient_DownloadStringCompleted;
+
+                String url = "http://api.openweathermap.org/data/2.5/forecast/daily?lat=" + lat + "&lon=" + lon + "&cnt=10&mode=json&units=metric";
+                weatherClient.DownloadStringAsync(new Uri(url));
+            }
+            catch (System.UnauthorizedAccessException)
+            {
+                MessageBox.Show("Couldn't get the geolocation");
+            }
+
+        }
+    }
+}
